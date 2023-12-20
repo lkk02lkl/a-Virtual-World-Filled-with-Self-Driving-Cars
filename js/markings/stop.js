25d106b7c292c6dcda9057fe921a4cc1c4b60e54
unknown <anhelinapavliuk@gmail.com>
2023-12-20 14:04:39 +0100

    added

--- js/markings/stop.js
+++ js/markings/stop.js
@@ -1,21 +1,12 @@
-class Stop {
+class Stop extends Marking {
   constructor(center, directionVector, width, height) {
-    this.center = center;
-    this.directionVector = directionVector;
-    this.width = width;
-    this.height = height;
-
-    this.support = new Segment(
-      translate(center, angle(directionVector), height / 2),
-      translate(center, angle(directionVector), -height / 2)
-    );
-    this.poly = new Envelope(this.support, width, 0).poly;
+    super(center, directionVector, width, height);
 
     this.border = this.poly.segments[2];
   }
 
   draw(ctx) {
-    this.border.draw(ctx, { width: 5, color: "white"});
+    this.border.draw(ctx, { width: 5, color: "white" });
     ctx.save();
     ctx.translate(this.center.x, this.center.y);
     ctx.rotate(angle(this.directionVector) - Math.PI / 2);
@@ -25,7 +16,7 @@ class Stop {
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.fillStyle = "white";
-    ctx.font = "bold" + this.height * 0.3 + "px Arial";
+    ctx.font = "bold " + this.height * 0.3 + "px Arial";
     ctx.fillText("STOP", 0, 1);
 
     ctx.restore();
