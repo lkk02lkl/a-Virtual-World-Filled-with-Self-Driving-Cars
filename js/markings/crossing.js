0e34a30acbc5cfd56006dd0b6460239510dd800a
unknown <anhelinapavliuk@gmail.com>
2023-12-20 13:27:56 +0100

    added

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -11,10 +11,19 @@ class Crossing {
     );
     this.poly = new Envelope(this.support, width, 0).poly;
 
-    this.border = this.poly.segments[2];
+    this.borders = [this.poly.segments[0], this.poly.segments[2]];
   }
 
   draw(ctx) {
-    this.poly.draw(ctx);
+    const perp = perpendicular(this.directionVector);
+    const line = new Segment(
+      add(this.center, scale(perp, this.width / 2)),
+      add(this.center, scale(perp, -this.width / 2))
+    );
+    line.draw(ctx, {
+      width: this.height,
+      color: "white",
+      dash: [11,11]
+    });
   }
 }
